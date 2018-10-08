79937114817b719e6071f98cb5db746093f77c25
Parth Shah <parthshahk@gmail.com>
2018-10-08 23:52:21 +0530

    Added Database words

--- getwords.php
+++ getwords.php
@@ -1,9 +1,21 @@
 <?php
-    $data= file_get_contents('words_dictionary.json');
-    $data = json_decode($data, true);
-    $words=array_keys($data);
     $hash=$_GET['q'];
     $hash=md5($hash);
     $i=str_split($hash,4);
-    echo $words[hexdec($i[0])*5.56]." ".$words[hexdec($i[1])*5.56]." ".$words[hexdec($i[2])*5.56]." ".$words[hexdec($i[3])*5.56];
+
+    $index1 = floor(hexdec($i[0])*5.50);
+    $index2 = floor(hexdec($i[1])*5.50);
+    $index3 = floor(hexdec($i[2])*5.50);
+    $index4 = floor(hexdec($i[3])*5.50);
+
+    // $con = mysqli_connect("localhost","parth","parth","test");
+    $con=mysqli_connect("mysql.hostinger.in","u696737897_parth","`7w4]8io`aR8xu1AB1","u696737897_datab");
+
+    $sql = "SELECT * FROM wordhash WHERE ID = $index1 OR ID = $index2 OR ID = $index3 OR ID = $index4";
+    $result = mysqli_query($con, $sql);
+
+    while($row = mysqli_fetch_assoc($result)) {
+
+        echo $row['Word']." ";
+     }
 ?>
--- index.php
+++ index.php
@@ -1,35 +1,12 @@
-<?php
-    $this_app="Word_Hashing";
-	
-    $con=mysqli_connect("mysql.hostinger.in","u696737897_parth","`7w4]8io`aR8xu1AB1","u696737897_datab");
-    $current_info_obj=mysqli_query($con,"SELECT * FROM visitors WHERE Alias='$this_app'");
-    $current_info=mysqli_fetch_array($current_info_obj,MYSQLI_NUM);
-    $current_users=$current_info[0];
-    $current_views=$current_info[1];
-    $current_usersN=$current_info[3];
-    $current_viewsN=$current_info[4];
-    $current_users++;
-    $current_views++;
-    $current_usersN++;
-    $current_viewsN++;
-    mysqli_query($con,"UPDATE visitors SET Views=$current_views,ViewsN=$current_viewsN WHERE Alias='$this_app'");
-
-    if(!isset($_COOKIE['parthshahk_'.$this_app.'_visit'])){
-
-        setcookie('parthshahk_'.$this_app.'_visit', '1', time() + 2592000, "/");
-        mysqli_query($con,"UPDATE visitors SET Users=$current_users,UsersN=$current_usersN WHERE Alias='$this_app'");
-    }
-?>
 <html>
     <head>
+        <?php include '../analytics.php'; ?>
         <title>Word Hashing</title>
         <meta charset="utf-8">
         <meta name="description" content="A hash function usually returns a hexadecimal value. Here in Word Hashing the function returns a set of 4 English words instead of a seemingly arbitrary set of characters. These words are selected by the program from a huge database of words using an existing hashing algorithm.">
-        <meta name="keywords" content="word hashing, hashing, hash function, parth shah">
-        <meta name="robots" content="index, follow">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="https://parthshah.xyz/resources/favicons/word_hashing.png">
-        <link rel="stylesheet" href="https://parthshah.xyz/resources/css/materialize.css">
+        <link rel="icon" type="image/png" href="favicon.png">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
         <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
         <style>
             .nav-wrapper{
@@ -84,7 +61,7 @@
     <body>
         <nav>
             <div class="nav-wrapper">
-                <a href="index.php" class="brand-logo center"><img src="https://parthshah.xyz/resources/images/word_hashing_header.png" height="70px"></a>
+                <a href="./" class="brand-logo center"><img src="word-hashing-header.png" height="70px"></a>
             </div>
         </nav>
         <div class="container">
@@ -106,12 +83,12 @@
         </div>
         <div class="row bottom center">
             <div class="col s12">
-                <p class="learn">What is this? <a href="learn.html"> Click Here</a> to learn.</p>
+                <p class="learn">What is this? <a href="learn.php"> Click Here</a> to learn.</p>
                 <p class="back">Go back to <a href="https://parthshah.xyz">Parth Shah - Projects</a></p>
             </div>
         </div>
-        <script src="https://parthshah.xyz/resources/javascript/jquery_3.2.1.js"></script>
-        <script src="https://parthshah.xyz/resources/javascript/materialize.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
         <script>
             function getWords(){
                 str=document.getElementById("text").value;
